636f427ee01eb599bcc2eabd1674024c199c6f40
jasonsanders <jas.sanders@gmail.com>
2016-10-27 21:02:50 -0600

    added livereload

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ let express = require('express');
 let cors = require('cors');
 let session = require('express-session');
 let bodyParser = require('body-parser');
+let liveReload = require('express-livereload');
 let app = express();
 let port = 8003;
 let User = require('./controllers/userCtrl');
 let Profile = require('./controllers/profileCtrl');
-
 
 app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.json());
@@ -15,9 +15,8 @@ app.use(session({secret: 'i have a secret'}));
 app.post('/api/login', User.login);
 app.get('/api/profiles', Profile.getFriends);
 
-
-
-
 app.listen(port, function() {
   console.log('Check it out, I\'m listening on port ' + port);
 });
+
+liveReload(app, config = {});
